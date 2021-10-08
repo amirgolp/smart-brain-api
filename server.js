881acea8dc203081fd4c3d234d5e4839f451c585
Amir Gol <golparvar.amir@gmail.com>
2021-10-08 11:57:51 +0200

    1

--- server.js
+++ server.js
@@ -33,6 +33,25 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(cors({origin: 'https://smart-brain-frontie.herokuapp.com/'}));
+// app.use(function (req, res, next) {
+
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'https://smart-brain-frontie.herokuapp.com/');
+
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
